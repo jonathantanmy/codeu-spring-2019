b573fce0baf38b6e11f2310a0cd86641c5fdb000
elaineliu99 <elaineliu99@gmail.com>
2019-03-02 01:23:57 -0500

    Finished Familiarization Project (Direct Messages)

--- src/main/webapp/js/user-page-loader.js
+++ src/main/webapp/js/user-page-loader.js
@@ -32,16 +32,19 @@ function setPageTitle() {
 /**
  * Shows the message form if the user is logged in and viewing their own page.
  */
-function showMessageFormIfViewingSelf() {
-  document.getElementById('about-me-form').classList.remove('hidden');
+
+ //Show the form as long as the user is logged in, even if they aren't viewing their own page
+ //Rename function to accurately reflect what it does
+function showMessageFormIfLoggedIn() {
+  document.getElementById('about-me-form').classList.remove('hidden'); //teammate doing About Me project added this line
   fetch('/login-status')
       .then((response) => {
         return response.json();
       })
       .then((loginStatus) => {
-        if (loginStatus.isLoggedIn &&
-            loginStatus.username == parameterUsername) {
+        if (loginStatus.isLoggedIn) {
           const messageForm = document.getElementById('message-form');
+          messageForm.action = '/messages?recipient=' + parameterUsername;  //add recipient parameter to the form's action attribute
           messageForm.classList.remove('hidden');
         }
       });
@@ -94,7 +97,7 @@ function buildMessageDiv(message) {
 /** Fetches data and populates the UI of the page. */
 function buildUI() {
   setPageTitle();
-  showMessageFormIfViewingSelf();
+  showMessageFormIfLoggedIn();
   fetchMessages();
   fetchAboutMe();
 }
