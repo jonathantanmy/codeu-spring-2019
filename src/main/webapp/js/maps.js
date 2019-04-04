7b0615d92202d60eaafafb8fc5fecbfba1e55834
Sandy <sxie@gatech.edu>
2019-04-04 14:40:58 -0400

    adding info windows to maps.js

--- src/main/webapp/js/maps.js
+++ src/main/webapp/js/maps.js
@@ -3,24 +3,28 @@ function createMap(){
       fetch('/MapsServlet').then(function(response) {
         return response.json();
       }).then((BubbleTeaLocations) => {
-
+        var manhattan = {lat: 40.7831, lng: -73.9712}
         const map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 40.7831, lng: -73.9712},
-          zoom:7
+          center: manhattan,
+          zoom:10
         });
 
         BubbleTeaLocations.forEach((BubbleTeaLocation) => {
-          new google.maps.Marker({
+          const marker = new google.maps.Marker({
             position: {lat: BubbleTeaLocation.lat, lng: BubbleTeaLocation.lng},
             map: map
           });
+          var name = BubbleTeaLocation.name
+          addInfoWindow(marker, name)
         });
       });
-
-      map.addListener('click', (event) => {
-        new google.maps.Marker({
-          position: event.latLng,
-          map: map
-        });
-      });
-    }
+}
+function addInfoWindow(map, marker, message) {
+  var infoWindow = new google.maps.InfoWindow({
+    content: name
+  });
+  google.maps.event.addListener('click', () => {
+    infoWindow.open(map, marker);
+  });
+  
+}
